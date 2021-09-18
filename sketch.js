01723fbf6f3daf6053352243506eea5abdd1116f
vedaanta1 <87821299+vedaanta1@users.noreply.github.com>
2021-09-18 10:56:11 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,110 @@
+var bg,sleep, brush, gym, eat, bath, move;
+var astronaut;
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-let engine;
-let world;
-var ball; 
-var ground
-var wedge;
-var angle = 70;
-
-
-function setup()
-{
-
-  createCanvas(400,400);
-  engine = Engine.create();
-  world = engine.world;
-  var ball_options = {
-    restitution: 0.95,
-    friction: 0.01
-  }
-  var ground_options = {
-    isStatic: true
-  }
-
-  var wedge_options = {
-    isStatic: true
-  }
-
-
-  ball = Bodies.circle(100, 25, 20, ball_options);
-  World.add(world, ball);
-  ground = Bodies.rectangle(200, 390, 400, 10, ground_options);
-  World.add(world, ground)
-  wedge = Bodies.rectangle(180, 100, 130, 20, wedge_options);
-  World.add(world, wedge)
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+function preload(){
+  bg= loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation("images/gym1.png","images/gym1.png","images/gym2.png","images/gym2.png");
+  eat = loadAnimation("images/eat1.png","images/eat1.png","images/eat1.png","images/eat2.png","images/eat2.png","images/eat2.png");
+  bath = loadAnimation("images/bath1.png","images/bath1.png","images/bath1.png","images/bath2.png","images/bath2.png","images/bath2.png");
+ move = loadAnimation("images/move1.png","images/move1.png","images/move2.png","images/move2.png");
 }
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  Matter.Body.rotate(wedge, angle);
-  push();
-  translate(wedge.position.x, wedge.position.y)
-  rotate(angle);
-  rect(0, 0, 100, 20);
-  pop();
-  angle = angle + 0.1
-  ellipse(ball.position.x, ball.position.y, 20);
-  rect(ground.position.x, ground.position.y, 400, 10);
-  fill("red");
-
- 
+function setup() {
+  createCanvas(600, 500);
+  
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
+  
 }
 
+function draw() {
+  background(bg);
+  drawSprites();
+
+  textSize(20);
+  fill("white")
+  text("Instructions:",20, 35);
+  textSize(15);
+  text("Up Arrow = Brushing",20, 55);
+  text("Down Arrow = Gymming",20, 70);
+  text("Left Arrow = Eating",20, 85);
+  text("Right Arrow = Bathing",20, 100);
+  text("m key = Moving",20, 115);
+  
+  edges=createEdgeSprites();
+  astronautbounce.Off(edges);
+
+  edges=createEdgeSprites();
+  astronaut.BounceOff(edges);
+
+  edges=createEdgeSprites();
+  astronaut.bounceOff(edges);
+
+  edges=createEdgeSprite();
+  astronaut.bounceOff(edges);
+  
+  if(keyDown("UP_ARROW")){
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  
+  if(keyDown("DOWN_ARROW")){
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  
+  if(keyDown("LEFT_ARROW")){
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating");
+    astronaut.x = 150;
+    astronaut.y = 350;
+    astronaut.velocityX = 0.5;
+    astronaut.velocityY = 0.5;
+  }
+  
+  if(keyDown("RIGHT_ARROW")){
+    astronaut.addAnimation("bathing", bath);
+    astronaut.changeAnimation("bathing");
+    astronaut.x = 300;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(key Down("m")){
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
+
+  if(key Down("m")){
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(key Down("m")){
+    astronaut.changeAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
+
+  if(key Down("m")){
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
+
+}
